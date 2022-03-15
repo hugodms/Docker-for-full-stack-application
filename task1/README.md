c6ab1f0ed5ecf12ef9b5d1b1e67b055f4d1a9195
hugodms <hugo.domingues@epitech.eu>
2022-03-15 21:12:43 +0100

    👷‍♂️ [DONE]

---
+++ task1/README.md
@@ -0,0 +1,13 @@
+# task 1
+
+## build 👷‍♂️
+
+```bash
+docker build -t react-test .
+```
+
+## run 🏃‍♂️
+
+```bash
+docker run -p 3000:3000 react-test
+```
---
+++ task1/src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.css';
+
+function Main() {
+
+  return (
+    <div className="Main">
+        <p>Welcome to React</p>
+    </div>
+  );
+}
+export default Main;
