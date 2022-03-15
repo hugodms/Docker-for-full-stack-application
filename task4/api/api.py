a8ebe48c1351a2b60e85d724a81548344e2a4bf8
hugodms <hugo.domingues@epitech.eu>
2022-03-16 00:06:18 +0100

    👷 task4 (update) [DONE]

--- task4/api/api.py
+++ task4/api/api.py
@@ -9,7 +9,7 @@ cors = CORS(app, resources={r"/foo": {"origins": "http://127.0.0.1:8080"}})
 @app.route("/")
 @cross_origin(origin='127.0.0.1',headers=['Content- Type','Authorization'])
 def hello():
-    return "Hello world!"
+    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False)
